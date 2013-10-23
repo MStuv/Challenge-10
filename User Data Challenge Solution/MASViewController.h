4e8fd4d789d491797dec034992e393ca653c8787
MStuv <markstuver@MacBook-Pro.local>
2013-10-22 22:33:12 -0400

    Initial Commit

---
+++ User Data Challenge Solution/MASViewController.h
@@ -0,0 +1,13 @@
+//
+//  MASViewController.h
+//  User Data Challenge Solution
+//
+//  Created by Mark Stuver on 10/22/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASViewController : UIViewController
+
+@end
